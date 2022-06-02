473f0a12b8954f27444707fb91abd9478587b5d9
Stephan Michard <git@michard.io>
2022-06-02 21:59:05 +0200

    variables file adjusted

--- 1_7_terraform_deployment/variables.tf
+++ 1_7_terraform_deployment/variables.tf
@@ -12,16 +12,6 @@ variable "region" {
   default = "europe-west3"
 }
 
-#variable "gke_username" {
-#  default     = ""
-#  description = "gke username"
-#}
-
-#variable "gke_password" {
-#  default     = ""
-#  description = "gke password"
-#}
-
 variable "gke_num_nodes" {
   description = "Number of gke nodes"
   default     = 1
